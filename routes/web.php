36e7e9344d225408b9bb425c42a286b63f1df681
Samim <Samim@gmail.com>
2025-11-10 14:11:36 +0430

    new route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -7,4 +9,20 @@ Route::get('/', function () {
 
 Route::get('main', function(){
     return "This is The world of Programming";
+});
+
+Route::get('/check-db', function() {
+    try {
+        DB::connection()->getPdo(); // Test DB connection
+        $tables = DB::select('SHOW TABLES'); // List all tables
+        return response()->json([
+            'status' => 'Connected!',
+            'tables' => $tables
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'status' => 'Failed!',
+            'error' => $e->getMessage()
+        ]);
+    }
 });
