86b4c3a4377e232dcf56c6b7b4548c876691323a
Samim <Samim@gmail.com>
2025-11-10 13:17:00 +0430

    new message

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('main', function(){
+    return "This is The world of Programming";
+});
