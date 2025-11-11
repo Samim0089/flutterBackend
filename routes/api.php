032e0e96fd4aa0ba257e36bd6fee203b1bda9751
Samim <Samim@gmail.com>
2025-11-11 12:15:07 +0430

    commit

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,10 @@
 use App\Http\Controllers\MytableController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/mytable', [MytableController::class, 'index']);
+Route::get('/mytable/{id}', [MytableController::class, 'show']);
+Route::post('/mytable', [MytableController::class, 'store']);
+Route::put('/mytable/{id}', [MytableController::class, 'update']);
+Route::delete('/mytable/{id}', [MytableController::class, 'destroy']);
 
-
-Route::get('/mytable', [MytableController::class, 'index']);        // List all
-Route::get('/mytable/{id}', [MytableController::class, 'show']);    // Single row
-Route::post('/mytable', [MytableController::class, 'store']);       // Add
-Route::put('/mytable/{id}', [MytableController::class, 'update']);  // Update
-Route::delete('/mytable/{id}', [MytableController::class, 'destroy']); // Delete
 
